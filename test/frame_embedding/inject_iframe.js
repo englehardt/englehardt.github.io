fe6dedcec5395744dc68cb5835e67bcbc7b96398
englehardt <englehardt@gmail.com>
2016-07-12 15:11:50 -0700

    Embedding an external script with a redirect

--- test/frame_embedding/inject_iframe.js
+++ test/frame_embedding/inject_iframe.js
@@ -1,3 +1,6 @@
 // For embedding into `embed_frame_with_script.html`
 var div = document.getElementById("external");
 div.innerHTML = div.innerHTML + "<iframe src='page3.html'></iframe>"
+
+var div = document.getElementById("external-img");
+div.innerHTML = div.innerHTML + "<img src='http://cs.princeton.edu/~ste/images/profile.jpg'>"
