4b060c4dfc5a72e04271c17e45c1071765c86f9c
englehardt <englehardt@gmail.com>
2016-07-13 15:24:02 -0700

    Adding additional test case to frame/script injection

--- test/frame_embedding/inject_iframe.js
+++ test/frame_embedding/inject_iframe.js
@@ -4,3 +4,8 @@ div.innerHTML = div.innerHTML + "<iframe src='page3.html'></iframe>"
 
 var div = document.getElementById("external-img");
 div.innerHTML = div.innerHTML + "<img src='http://cs.princeton.edu/~ste/images/profile.jpg'>"
+
+function functionInInjectedScript() {
+  var div = document.createElement("div");
+  div.innerHTML = "<img src='https://people.mozilla.org/PeopleOfMozilla.jpg'>";
+}
