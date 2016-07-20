e2e3f10d5b7a2e69fd803024985e9fcaba930484
englehardt <englehardt@gmail.com>
2016-07-20 16:53:12 -0700

    Adding a few more iframe tests

---
+++ test/frame_embedding/inject_self_navigating_iframe.js
@@ -0,0 +1,3 @@
+iframe = document.createElement('iframe');
+iframe.onload = function(){this.contentDocument.location = 'page5.html'}
+document.body.appendChild(iframe)
---
+++ test/frame_embedding/navigate_iframe.js
@@ -0,0 +1,3 @@
+// For embedding into `embed_frame_with_script.html`
+var iframe = document.getElementById("page4");
+iframe.contentDocument.location = 'page4.html';
