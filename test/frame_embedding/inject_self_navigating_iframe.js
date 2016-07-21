18780fe947e0972664748a77f82ae2379b8b5429
englehardt <englehardt@gmail.com>
2016-07-20 17:00:15 -0700

    bugfix

--- test/frame_embedding/inject_self_navigating_iframe.js
+++ test/frame_embedding/inject_self_navigating_iframe.js
@@ -1,3 +1,3 @@
 iframe = document.createElement('iframe');
-iframe.onload = function(){this.contentDocument.location = 'page5.html'}
+iframe.onload = function(){this.onload=''; this.contentDocument.location = 'page5.html'}
 document.body.appendChild(iframe)
