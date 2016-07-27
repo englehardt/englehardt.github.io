3a82b5558ae462ded6d902c93f88209414ed27d1
englehardt <englehardt@gmail.com>
2016-07-26 23:24:25 -0700

    insert image

--- test/frame_embedding/inject_so_iframe.js
+++ test/frame_embedding/inject_so_iframe.js
@@ -1,5 +1,10 @@
 // For embedding into `embed_frame_with_script.html`
 var iframe = document.createElement("iframe");
-iframe.src = "javascript:\'<html><body style='background:transparent'></body></html>\';"
-iframe.id = "javascript-src-url"
-document.body.appendChild(iframe)
+iframe.src = "javascript:\'<html><body style='background:transparent'></body></html>\';";
+iframe.id = "javascript-src-url";
+document.body.appendChild(iframe);
+
+var elem = document.getElementById("javascript-src-url");
+var img = elem.contentDocument.createElement("img");
+img.src = "blah.jpg";
+elem.contentDocument.body.appendChild(img);
