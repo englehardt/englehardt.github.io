14b2c89ba973cc8577d7d2a31c258e3d9777ff08
englehardt <englehardt@gmail.com>
2016-07-26 23:13:36 -0700

    bugfix

--- test/frame_embedding/inject_so_iframe.js
+++ test/frame_embedding/inject_so_iframe.js
@@ -1,5 +1,5 @@
 // For embedding into `embed_frame_with_script.html`
 var iframe = document.createElement("iframe");
-iframe.src = "javascript:'<html><body style='background:transparent'></body></html>';"
+iframe.src = "javascript:\'<html><body style='background:transparent'></body></html>\';"
 iframe.id = "javascript-src-url"
 document.body.appendChild(iframe)
