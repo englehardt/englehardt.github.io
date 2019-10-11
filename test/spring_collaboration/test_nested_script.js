ff00a4b094ccd434193ca4b2ab2daa95ae382815
englehardt <englehardt@gmail.com>
2019-10-10 21:20:57 -0700

    Remove infinite loop

--- test/spring_collaboration/test_nested_script.js
+++ test/spring_collaboration/test_nested_script.js
@@ -2,6 +2,7 @@ window.onload = function() {
 	var iframe = document.createElement("iframe");
 	iframe.addEventListener('load', function(e) {
     this.src = 'https://englehardt-tracker.com/spring_collaboration/test_nested.html'
+    this.removeEventListener('load', arguments.callee, false);
   }, false);
 	document.body.appendChild(iframe);
 }
